d0a151f63ff45f3c9e7b849f953e07b5c34ffe67
josimarviana <josimarviana@gmail.com>
2022-11-14 19:26:41 -0300

    tela signup

--- lib/src/auth/components/custom_text_field.dart
+++ lib/src/auth/components/custom_text_field.dart
@@ -6,11 +6,13 @@ class CustomTextField extends StatefulWidget {
   final IconData icon;
   final String label;
   final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
 
   const CustomTextField({
     required this.icon,
     required this.label,
     this.isSecret = false,
+    this.inputFormatters,
     super.key
   });
 
@@ -34,6 +36,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
+      inputFormatters: widget.inputFormatters,
       obscureText: isObscure,
       decoration: InputDecoration(
         prefixIcon: Icon(widget.icon),
--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:quitandavirtual/src/auth/components/custom_text_field.dart';
 import 'package:quitandavirtual/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+  SignUpScreen({super.key});
+
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
+
+final phoneFormatter = MaskTextInputFormatter(
+    mask: '(##) # ####-####',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +64,15 @@ class SignUpScreen extends StatelessWidget {
                         icon: Icons.person,
                          label: 'Nome',
                          ),
-                      const CustomTextField(
+                      CustomTextField(
                         icon: Icons.phone,
                          label: 'Celular',
+                         inputFormatters: [phoneFormatter],
                          ),
-                      const CustomTextField(
+                      CustomTextField(
                         icon: Icons.file_copy,
                          label: 'CPF',
+                         inputFormatters: [cpfFormatter],
                          ),
                       SizedBox(
                         height: 50,
