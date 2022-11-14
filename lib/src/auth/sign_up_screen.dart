313038454881704f52fe74d953d68168a293b0dc
josimarviana <josimarviana@gmail.com>
2022-11-14 19:01:49 -0300

    botao de voltar

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:quitandavirtual/src/auth/components/custom_text_field.dart';
 import 'package:quitandavirtual/src/config/custom_colors.dart';
@@ -15,72 +14,92 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(children: [
-            const Expanded(
-              child: Center(
-                child: Text('Cadastro', style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                ),),
-              ),
-            ),
-            //formulario
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 40,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
+          child: Stack(
+            children: [
+              Column(children: [
+                const Expanded(
+                  child: Center(
+                    child: Text('Cadastro', style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                    ),),
+                  ),
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const CustomTextField(
-                    icon: Icons.email,
-                     label: 'Email',
-                     ),
-                  const CustomTextField(
-                    icon: Icons.lock,
-                     label: 'Senha',
-                      isSecret: true,
-                      ),
-                  const CustomTextField(
-                    icon: Icons.person,
-                     label: 'Nome',
-                     ),
-                  const CustomTextField(
-                    icon: Icons.phone,
-                     label: 'Celular',
-                     ),
-                  const CustomTextField(
-                    icon: Icons.file_copy,
-                     label: 'CPF',
-                     ),
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      ),
-                      onPressed: (){}, 
-                       child: const Text(
-                        'Cadastrar Usuário',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                        ),
-                      ),
-                  )
-              ]),
-            ),
+                //formulario
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 40,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(45),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const CustomTextField(
+                        icon: Icons.email,
+                         label: 'Email',
+                         ),
+                      const CustomTextField(
+                        icon: Icons.lock,
+                         label: 'Senha',
+                          isSecret: true,
+                          ),
+                      const CustomTextField(
+                        icon: Icons.person,
+                         label: 'Nome',
+                         ),
+                      const CustomTextField(
+                        icon: Icons.phone,
+                         label: 'Celular',
+                         ),
+                      const CustomTextField(
+                        icon: Icons.file_copy,
+                         label: 'CPF',
+                         ),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          onPressed: (){}, 
+                           child: const Text(
+                            'Cadastrar Usuário',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                            ),
+                          ),
+                      )
+                  ]),
+                ),
+                
+              ],),
             
-          ],),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    },
+                     icon: const Icon(
+                      Icons.arrow_back_ios),
+                      color: Colors.white,
+                      ),
+                      ),
+              ),
+              
+            ],
+          ),
         ),
       )
     );
